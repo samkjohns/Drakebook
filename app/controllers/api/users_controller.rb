8cd396d5e4d0b1c77ee93afe32e5bed550cd9e4a
Sam Johns <samkjohns@gmail.com>
2016-06-02 15:48:35 -0400

    Removes debugger line

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,7 +6,6 @@ class Api::UsersController < ApplicationController
       login @user
       render :show
     else
-      debugger
       render(
         json: { errors: @user.errors.full_messages },
         status: 422
